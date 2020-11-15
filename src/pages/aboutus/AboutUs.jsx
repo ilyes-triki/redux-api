aa25db759b0cd2b64a21ef6a456e10045d96be94
ilyes triki <garouchenoob@gmail.com>
2020-11-15 08:45:08 +0100

    third commit

--- src/pages/aboutus/AboutUs.jsx
+++ src/pages/aboutus/AboutUs.jsx
@@ -1,7 +1,33 @@
 import React from "react";
-
+import "./AboutUs.css";
 function AboutUs() {
-  return <div>AboutUs</div>;
+  return (
+    <div className="page">
+      <div className="content">
+        <img src="\téléchargé.jpg" alt="bar pic" />
+      </div>
+      <div className="content">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut nulla non
+          blanditiis nisi unde accusantium iure deserunt saepe, numquam,
+          explicabo ipsam expedita quasi architecto ipsum a quibusdam
+          dignissimos voluptas facere!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+          laboriosam ex consequuntur nostrum necessitatibus unde ipsa et? Sint
+          illum nisi nostrum. Enim reprehenderit quia dignissimos fugiat quos
+          odio, perferendis magni!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quis
+          dignissimos possimus, dolorem vitae aspernatur harum laborum
+          architecto pariatur est corrupti deleniti inventore alias incidunt!
+          Quo quaerat saepe adipisci quas.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default AboutUs;
