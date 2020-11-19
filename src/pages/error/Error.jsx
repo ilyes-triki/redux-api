bd4111acd00224c2edefa658d915545a892f1e40
ilyes triki <garouchenoob@gmail.com>
2020-11-19 10:13:20 +0100

    third commit

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Error() {
-  return <div>Error 404</div>;
+  return (
+    <div>
+      <img
+        height="100%"
+        width="100%"
+        className="error-img"
+        src="https://miro.medium.com/max/800/1*hFwwQAW45673VGKrMPE2qQ.png"
+        alt="error"
+      />
+    </div>
+  );
 }
 
 export default Error;
